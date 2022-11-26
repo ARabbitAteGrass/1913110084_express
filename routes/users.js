4bbef3c4b778964dd1397d17870e9c9b3b5971f3
arabbitategrass <mooksanmook@gmail.com>
2022-11-26 11:22:44 +0700

    Workshop A

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('hello world');
+  res.status(200).json({
+    fullname: 'Sanmook Nantasukon'
+  })
+});
+
+router.get('/bio', function(req, res, next) {
+  // res.send('hello world');
+  res.status(200).json({
+    fullname: 'Sanmook Nantasukon',
+    nickname: "mook",
+    hobby: "sleep",
+    gitusername: "arabbitategrass"
+  })
 });
 
 module.exports = router;
