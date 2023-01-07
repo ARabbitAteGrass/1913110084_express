a5a995eecf3c2d300c6cd57b4c0e0defc7b8b524
MookieS <50792459+ARabbitAteGrass@users.noreply.github.com>
2023-01-07 12:10:18 +0700

    Week 6 Workshop B

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,6 +1,11 @@
 const { shop } = require("../models/shop");
 const menu = require("../models/menu");
 const config = require('../config/index')
+const fs = require('fs');
+const path = require('path');
+const uuidv4 = require('uuid');
+const { promisify } = require('util')
+const writeFileAsync = promisify(fs.writeFile)
 
 const index = async (req, res, next) => {
   const shops = await shop.find().sort({_id:-1});
@@ -9,7 +14,7 @@ const index = async (req, res, next) => {
     return{
       id: shop._id,
       name: shop.name,
-      photo: config.DOMAIN + shop.photo,
+      photo: config.DOMAIN + '/images/' + shop.photo,
       location: shop.location
     }
   })
@@ -30,4 +35,64 @@ const getShopMenu = async (req, res, next) => {
 
 }
 
-module.exports = { index: index ,getMenu, getShopMenu};
+const addShop = async (req, res, next) => {
+  const { name, location, photo } = req.body;
+  let shopinsert = shop({
+    name: name,
+    location: location,
+    photo: await saveImageToDisk(photo),
+  });
+  const result = await shopinsert.save();
+  return res
+    .status(201)
+    .json({ message: `Insert Successful: ${(result != null)}` });
+};
+
+async function saveImageToDisk(baseImage) {
+  //หา path จริงของโปรเจค
+  const projectPath = process.cwd();
+  //โฟลเดอร์และ path ของการอัปโหลด
+  const uploadPath = `${projectPath}/public/images/`;
+
+  //หานามสกุลไฟล์
+  const ext = baseImage.substring(
+    baseImage.indexOf("/") + 1,
+    baseImage.indexOf(";base64")
+  );
+
+  //สุ่มชื่อไฟล์ใหม่ พร้อมนามสกุล
+  let filename = "";
+  if (ext === "svg+xml") {
+    filename = `${uuidv4.v4()}.svg`;
+  } else {
+    filename = `${uuidv4.v4()}.${ext}`;
+  }
+
+  //Extract base64 data ออกมา
+  let image = decodeBase64Image(baseImage);
+
+  //เขียนไฟล์ไปไว้ที่ path
+  await writeFileAsync(uploadPath + filename, image.data, "base64");
+  //return ชื่อไฟล์ใหม่ออกไป
+  return filename;
+}
+
+function decodeBase64Image(base64Str) {
+  var matches = base64Str.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+  var image = {};
+  if (!matches || matches.length !== 3) {
+    throw new Error("Invalid base64 string");
+  }
+
+  image.type = matches[1];
+  image.data = matches[2];
+
+  return image;
+}
+
+module.exports = {
+  index: index,
+  menu: getMenu,
+  shop: getShop,
+  insert: addShop,
+};
