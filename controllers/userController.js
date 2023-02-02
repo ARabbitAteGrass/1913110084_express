12bfd9dbdd88bb55ce43591d55ddff816c577ca8
arabbitategrass <mooksanmook@gmail.com>
2023-02-02 14:27:02 +0700

    Week 8 Workshop B

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require("../models/user")
 const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
 
 exports.index = (req, res, next) => {
     // res.send('hello world');
@@ -52,4 +54,56 @@ exports.register = async(req, res, next) => {
     next(error)
   }
 }
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error("ข้อมูลที่ได้รับมาไม่ถูกต้อง");
+      error.statusCode = 422;
+      error.validation = errors.array();
+      throw error;
+    }
+
+    // check email isExist
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const error = new Error("ไม่พบผู้ใช้งาน");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const isValid = await user.checkPassword(password);
+    if (!isValid) {
+      const error = new Error("รหัสผ่านไม่ถูกต้อง");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    // token
+    const token = await jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      config.JWT_SECRET,
+      {
+        expiresIn: "5 days",
+      }
+    );
+
+    const expires_in = jwt.decode(token);
+
+    res.status(200).json({
+      access_token: token,
+      expires_in: expires_in.exp,
+      token_type: "Bearer",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
